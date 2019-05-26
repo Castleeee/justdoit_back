055a7a97609e4162d8b1e31ea51f3ec319f5ab1f
“owl <576359186@qq.com>
2019-05-26 23:10:31 +0800

    正在做用户

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,9 +2,13 @@
 from django.urls import path
 #from .views import
 from rest_framework.authtoken import views
+from rest_framework_jwt.views import obtain_jwt_token,refresh_jwt_token
+
 app_name = 'users'
 urlpatterns = [
-
+    path(r'login/', obtain_jwt_token),  # jwt的token验证
+    path(r'tokenRefresh/', refresh_jwt_token),  # token刷新   
+    #path(r"")
 ]
 # path(r'register/', views.register, name='register'),#注册
 # path(r'login/', views.login, name='login'),#登录
